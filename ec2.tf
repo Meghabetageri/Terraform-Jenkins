6501323943a699b7e0ad50f3e79ffd8242ffe941
meghabetageri <meghabetageri126@gmail.com>
2025-02-10 23:15:28 -0800

    pushing ec2 to github

---
+++ ec2.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "ap-south-1"  # Replace with your desired AWS region
+}
+
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-00bb6a80f01f03502"  # Replace with a valid AMI ID for your region
+  instance_type = "t2.micro"  # Instance type
+  
+  tags = {
+    Name = "MyTerraformEC2"
+  }
+}
